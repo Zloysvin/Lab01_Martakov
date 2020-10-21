08175d24201ee84a8a409e76ac51bbd300a85f34
данил мартаков <danylomartakov@gmail.com>
2020-10-21 15:37:30 +0300

    Добавьте файлы проекта.

---
+++ Lab1OP_Martakov/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab1OP_Martakov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grad = Convert.ToInt32(Console.ReadLine());
+            int minutes = grad % 30 * 2;
+            int hours = grad / 30;
+            Console.WriteLine($"{hours}h {minutes}m");
+        }
+    }
+}
